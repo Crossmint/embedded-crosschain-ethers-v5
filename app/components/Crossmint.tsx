a31639d6250f9e00dc96d20c0dcbad77521b9ffe
Danny Mulvihill <danny@paella.dev>
2024-01-24 18:24:14 -0800

    cleanup crossmint file with info from max

--- app/components/Crossmint.tsx
+++ app/components/Crossmint.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { CrossmintPaymentElement } from "@crossmint/client-sdk-react-ui";
 import { ethers } from "ethers";
 import Minting from "./Minting";
@@ -13,30 +13,26 @@ declare global {
 
 const Crossmint: React.FC = () => {
   const [orderIdentifier, setOrderIdentifier] = useState<string | null>(null);
-  const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
-  const [signer, setSigner] = useState<any>();
-  const [transactionHash, setTransactionHash] = useState("");
+  const [accounts, setAccounts] = useState([]);
+  const signerRef = useRef<ethers.providers.JsonRpcSigner>();
 
-  const projectId = process.env.NEXT_PUBLIC_CROSSMINT_PROJECT_ID as string;
-  const collectionId = process.env
-    .NEXT_PUBLIC_CROSSMINT_COLLECTION_ID as string;
-  const environment = process.env.NEXT_PUBLIC_CROSSMINT_ENVIRONMENT as string;
+  const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
+  const collectionId = process.env.NEXT_PUBLIC_COLLECTION_ID as string;
+  const environment = process.env.NEXT_PUBLIC_ENVIRONMENT as string;
 
   useEffect(() => {
     const initializeEthers = async () => {
       if (typeof window.ethereum !== "undefined") {
         try {
-          // Request account access
-          await window.ethereum.request({ method: "eth_requestAccounts" });
+          const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          setAccounts(accounts);
 
-          // Initialize the provider
           const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-          // Get the signer
           const signer = provider.getSigner();
-
-          console.log("Ethers provider and signer initialized");
-          return { provider, signer };
+          signerRef.current = signer;
         } catch (error) {
           console.error("User denied account access", error);
         }
@@ -58,22 +54,18 @@ const Crossmint: React.FC = () => {
             environment={environment}
             paymentMethod="ETH"
             signer={{
-              address: "0x6C3b3225759Cbda68F96378A9F0277B4374f9F06", // metamask wallet address
+              address: accounts[0],
               signAndSendTransaction: async (transaction) => {
-                console.log("signAndSendTransaction called");
-                console.log("transaction object:", transaction);
-                const txn = {
-                  to: "0xa105C311fA72b8Fb78c992EcbDb8b02Ea5bd394d",
-                  from: "0x6C3b3225759Cbda68F96378A9F0277B4374f9F06",
-                  value: BigInt(1000000000000000),
-                };
-                console.log("about to send request");
-                const { hash } = await window.ethereum.request({
-                  method: "eth_sendTransaction",
-                  params: [transaction],
+                if (!signerRef.current) {
+                  throw new Error("Signer is not initialized");
+                }
+
+                const response = await signerRef.current.sendTransaction({
+                  ...transaction,
+                  type: transaction.type!,
                 });
-                console.log("about to return hash");
-                return hash;
+
+                return response.hash;
               },
             }}
             mintConfig={{
